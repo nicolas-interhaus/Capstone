2e7f1ddfdbc8a78f977ac01521cdcf14eff73a12
Nicolas zamorano <nzamorano@interhaus.com>
2024-10-03 17:16:53 -0300

    actualizacion de las variables

--- Proyecto Vecinos Unidos/modelo/eventos.py
+++ Proyecto Vecinos Unidos/modelo/eventos.py
@@ -1,8 +1,11 @@
 class Eventos:
-    def __init__(self, nombre, direccion, costo):
+    def __init__(self, nombre,fec_evento,patrocinadores, direccion_evento, precio_evento,capacidad_evento):
         self.nombre = nombre
-        self.direccion = direccion
-        self.costo = costo
+        self.fec_evento = fec_evento
+        self.patrocinadores = patrocinadores
+        self.direccion_evento = direccion_evento
+        self.precio_evento = precio_evento
+        self.capacidad_evento = capacidad_evento
 
     def __str__(self):
         return f"Usuario: {self.nombre}, Edad: {self.edad}, Correo: {self.correo}" 
--- Proyecto Vecinos Unidos/modelo/vecinos.py
+++ Proyecto Vecinos Unidos/modelo/vecinos.py
@@ -1,8 +1,15 @@
 class Usuario:
-    def __init__(self, nombre, edad, correo):
-        self.nombre = nombre
+    def __init__(self, nombres,apellido_paterno,apellido_materno,fec_nacimiento,edad,genero,direccion,comuna,cargo,email):
+        self.nombres = nombres
+        self.apellido_paterno = apellido_paterno
+        self.apellido_materno = apellido_materno
+        self.fec_nacimiento = fec_nacimiento
         self.edad = edad
-        self.correo = correo
+        self.genero = genero
+        self.direccion = direccion
+        self.comuna = comuna
+        self.cargo = cargo
+        self.email = email
 
     def __str__(self):
         return f"Usuario: {self.nombre}, Edad: {self.edad}, Correo: {self.correo}"
